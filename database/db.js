1548fde391358677bb6317415994ae1fa89656cb
arsalanahmad123 <arsalanahmad15092003@gmail.com>
2024-06-28 16:47:06 +0500

    changed some files

--- database/db.js
+++ database/db.js
@@ -1,12 +1,25 @@
 const mongoose = require('mongoose')
 
+const Counter = require('../models/counter.model')
+
+async function initializeCounter() {
+    const counter = await Counter.findById('invoiceid')
+    if (!counter) {
+        const newCounter = new Counter({
+            _id: 'invoiceid',
+            sequence_value: 1000,
+        })
+        await newCounter.save()
+        console.log('Initialized invoiceid counter to 1000')
+    } else {
+        console.log('Counter already exists:', counter)
+    }
+}
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(
-            process.env.MONGO_URI ||
-                'mongodb://ahmad:ahmad@cluster0.jaucf3x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-        )
+        const conn = await mongoose.connect(process.env.MONGO_URI)
         console.log(`MongoDB Connected: ${conn.connection.host}`)
+        initializeCounter()
     } catch (error) {
         console.log(error)
     }
